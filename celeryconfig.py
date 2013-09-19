5d671f4e2a534a42417c4e811097a54697b31fbb
Eric van Zanten <eric@bahai.us>
2013-09-18 19:12:06 -0500

    Changed gunicorn version

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,4 +1,12 @@
 import os
+from datetime import timedelta
 
 BROKER_URL = 'sqs://%s:%s@' % (os.environ['AWS_ACCESS_KEY'], os.environ['AWS_SECRET_KEY'])
 CELERY_IMPORTS = ('worker', )
+
+CELERYBEAT_SCHEDULE = {
+    'grab-every-5-mins': {
+        'task': 'worker.update_all_projects',
+        'schedule': timedelta(seconds=300),
+    },
+}
