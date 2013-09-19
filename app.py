d73b228d787085f028216d0738faa465c899fbaa
Eric van Zanten <eric@bahai.us>
2013-09-19 14:34:37 -0500

    Removed unnecessary get param from update path

--- app.py
+++ app.py
@@ -76,21 +76,18 @@ def get_projects():
 
 @app.route('/update-projects/', methods=['GET'])
 def update_projects():
-    if request.args.get('the_key') == THE_KEY:
-        details = []
-        with open('projects.json', 'rb') as f:
-            projects = json.loads(f.read())
-            for project in projects:
-                # Call task below as normal function so processing
-                # is not delayed.
-                pj_details = update_project(project)
-                if pj_details:
-                    details.append(pj_details)
-        with open('project_details.json', 'wb') as f:
-            f.write(json.dumps(details))
-        resp = make_response('woot')
-    else:
-        resp = make_response("I can't do that, Dave", 401)
+    details = []
+    with open('projects.json', 'rb') as f:
+        projects = json.loads(f.read())
+        for project in projects:
+            # Call task below as normal function so processing
+            # is not delayed.
+            pj_details = update_project(project)
+            if pj_details:
+                details.append(pj_details)
+    with open('project_details.json', 'wb') as f:
+        f.write(json.dumps(details))
+    resp = make_response('woot')
     return resp
 
 def update_project(project_url):
